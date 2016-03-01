f82b8ba66412cf9980a11454b3602eb54c5e704f
gnogueira <~gust.oliveira@gmail.com~>
2016-03-01 19:40:49 -0300

    Commit Inicial - Estrutura

---
+++ src/bathroom/ObjectDetector.py
@@ -0,0 +1,17 @@
+'''
+Created on 29 de fev de 2016
+
+@author: gnogueira
+'''
+
+class MyClass(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
