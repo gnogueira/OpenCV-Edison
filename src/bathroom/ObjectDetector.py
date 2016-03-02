d40d4a0657e6427ef12304112167a31f015c787b
Gustavo <gust.oliveira@gmail.com>
2016-03-02 20:39:39 -0300

    Inicio ObjectDetector
    Falta para detecção do objeto:
    - Terminar detecção
    - Verificar lentidção

--- src/bathroom/ObjectDetector.py
+++ src/bathroom/ObjectDetector.py
@@ -1,44 +1,100 @@
+#!/usr/bin/env python
+
 import numpy as np
 import cv2
+
+import sys, getopt
 
 '''
 Created on 29 de fev de 2016
 
 @author: gnogueira
 '''
+from twisted.internet.process import detector
 
-class MyClass(object):
+class ObjectDetector(object):
     '''
     classdocs
     '''
+    detector = None
+    matcher = None
+    imgObject = None
+    
 
 
-    def __init__(self, params):
+    def __init__(self, method_name, imgObjectPath):
         '''
         Constructor
         '''
+        try:
+            self.imgObject = cv2.imread(imgObjectPath, 0)
+        except:
+            print "ERRO: Selecione a imagem para comparacao"
+            sys.exit(1)     
+        
+        self.init_detector(method_name)
+        if detector is None:
+            print "ERRO: Selecione um metodo valido [sift/surf/orb/akaze/brisk]"
+            sys.exit(1)               
+        
+        print "Usando metodo ", method_name
+        
     
-    
-    def init_feature(self, name):
-        chunks = name.split('-')
+    def init_detector(self, method_name):
+        
+        chunks = method_name.split('-')
         if chunks[0] == 'sift':
-            detector = cv2.xfeatures2d.SIFT_create()
+            self.detector = cv2.xfeatures2d.SIFT_create()
             norm = cv2.NORM_L2
         elif chunks[0] == 'surf':
-            detector = cv2.xfeatures2d.SURF_create(800)
+            self.detector = cv2.xfeatures2d.SURF_create(800)
             norm = cv2.NORM_L2
         elif chunks[0] == 'orb':
-            detector = cv2.ORB_create(400)
+            self.detector = cv2.ORB_create(400)
             norm = cv2.NORM_HAMMING
         elif chunks[0] == 'akaze':
-            detector = cv2.AKAZE_create()
+            self.detector = cv2.AKAZE_create()
             norm = cv2.NORM_HAMMING
         elif chunks[0] == 'brisk':
-            detector = cv2.BRISK_create()
+            self.detector = cv2.BRISK_create()
             norm = cv2.NORM_HAMMING
+        
+        if self.detector is not None:
+            self.matcher = cv2.BFMatcher(norm)
+    
+    def detect_object(self, imgScene):
+               
+        kp1, desc1 = self.detector.detectAndCompute(self.imgObject,None)
+        kp2, desc2 = self.detector.detectAndCompute(imgScene, None)
+        
+        print 'img1 - %d features, img2 - %d features' % (len(kp1), len(kp2))
+        
+        print 'Comparando...'
+        raw_matches = self.matcher.knnMatch(desc1, trainDescriptors = desc2, k = 2)
+        p1, p2, kp_pairs = self.filter_matches(kp1, kp2, raw_matches)
+        if len(p1) >= 4:
+            H, status = cv2.findHomography(p1, p2, cv2.RANSAC, 5.0)
+            print '%d / %d  inliers/matched' % (np.sum(status), len(status))
         else:
-            return None, None
+            H, status = None, None
+            print '%d matches found, not enough for homography estimation' % len(p1)
         
-        matcher = cv2.BFMatcher(norm)
+        # TODO: Terminar comparação
+        #vis = explore_match(win, img1, img2, kp_pairs, status, H)
         
-        return detector, matcher    
+        
+    def filter_matches(self, kp1, kp2, matches, ratio = 0.75):
+        mkp1, mkp2 = [], []
+        for m in matches:
+            if len(m) == 2 and m[0].distance < m[1].distance * ratio:
+                m = m[0]
+                mkp1.append( kp1[m.queryIdx] )
+                mkp2.append( kp2[m.trainIdx] )
+        p1 = np.float32([kp.pt for kp in mkp1])
+        p2 = np.float32([kp.pt for kp in mkp2])
+        kp_pairs = zip(mkp1, mkp2)
+        return p1, p2, kp_pairs
+        
+        
+        
+    
--- src/bathroom/__init__.py
+++ src/bathroom/__init__.py
@@ -1,19 +1,40 @@
+#!/usr/bin/env python
 
 import numpy as np
 import cv2
+from ObjectDetector import ObjectDetector
+
+#------------ CONSTANTES - INICIO ------------
+
+#Metodo que sera usado na comparacao do objeto
+MATCH_METHOD = "sift"
+
+#Caminho da imagem base de comparacao do objeto
+IMG_SOURCE = "../../resources/banheiro_fora_de_uso.png"
+
+#------------ CONSTANTES - FIM ------------
 
 cap = cv2.VideoCapture(0)
 
 while(True):
     # Capta o video
-    ret, frame = cap.read()
-
-    # TODO: Fazer os tratamentos e reconhecimentos aqui - INICIO
+    retornoCam, frame = cap.read()
     
-    # Utilizando SIFT para reconhecimento do Objeto
-    #detector = ObjectDetector.
+    # se nao conseguiu ler o video, sai
+    if retornoCam is None:
+        break
     
-    # Fazer os tratamentos e reconhecimentos aqui - FIM
+    # --- Tratamentos e Reconhecimentos - INICIO
+    
+    # Detectando o Objeto
+    # TODO: esta muito lento. Verificar.
+    detector = ObjectDetector(MATCH_METHOD,IMG_SOURCE)
+    detector.detect_object(frame)
+    
+    #Detectando e Contando Pessoas
+    # TODO: fazer tratamento
+    
+    # --- Tratamentos e Reconhecimentos - FIM
     
 
     # Mostra o resultado
