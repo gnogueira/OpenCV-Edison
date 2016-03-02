107b3a081b2ae27d5f8532f54578b3d274575da4
Gustavo <gust.oliveira@gmail.com>
2016-03-01 23:38:42 -0300

    Corrigindo problema upside down

--- src/bathroom/__init__.py
+++ src/bathroom/__init__.py
@@ -1,23 +1,24 @@
+
 import numpy as np
 import cv2
 
-# abre o video
 cap = cv2.VideoCapture(0)
 
-#le o primeiro frame
-ret,frame = cap.read()
+while(True):
+    # Capta o video
+    ret, frame = cap.read()
 
-#mostra o video
-while(1):
-    ret,frame = cap.read()
+    # TODO: Fazer os tratamentos e reconhecimentos aqui - INIICIO
     
-    if ret == True:
-        frame = cv2.flip(frame,0)
-        cv2.imshow('Frame',frame)
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-    else:
+    
+    
+    # Fazer os tratamentos e reconhecimentos aqui - INIICIO
+    
+
+    # Mostra o resultado
+    cv2.imshow('frame',frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-#libera as entradas
+
 cap.release()
 cv2.destroyAllWindows()
